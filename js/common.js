2e0f1ae9242fe95abbef684d528dfdc51bcbe37e
Dreg0 <andrii.kruglakov@gmail.com>
2017-07-23 16:47:38 +0300

    rearranged code in html adn css, js-script linked in header, where are two css files:glogal for whole page, and for header, at the top oh header css - listed styles, what can be applied to page in general. For now where is now @media positioning

--- js/common.js
+++ js/common.js
@@ -1,9 +1,9 @@
-  function openSlideMenu(){
-      document.getElementById('side-menu').style.width = '250px';
+ function openSlideMenu(){
+      document.getElementById('header-side-menu').style.width = '250px';
     
     }
 
     function closeSlideMenu(){
-      document.getElementById('side-menu').style.width = '0';
+      document.getElementById('header-side-menu').style.width = '0';
     
     }
